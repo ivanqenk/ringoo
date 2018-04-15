4b295d76bda98cbba3b17d94c7308968fd5e1cae
Ivan <icuenca@netwarmonitor.com>
2018-04-15 02:20:43 -0500

    ++

--- models/connection_sqli.php
+++ models/connection_sqli.php
@@ -88,4 +88,4 @@
 		}
 		//Termina transaccion-----------
 	}
-?>
+
--- models/index.php
+++ models/index.php
@@ -2,14 +2,14 @@
 //require("models/connection.php"); // funciones mySQL 
 require("models/connection_sqli.php"); // funciones mySQLi
 
-	class IndexModel extends Connection
+class IndexModel extends Connection
+{
+	public function metodo($val)
 	{
-		public function metodo($val)
-		{
-			$myQuery = "SELECT fecha_pago FROM app_pagos WHERE id = $val";
-			$res = $this->query($myQuery);
-			$res = $res->fetch_object();
-			
-			return $res->fecha_pago;
-		}
+		$myQuery = "SELECT fecha_pago FROM app_pagos WHERE id = $val";
+		$res = $this->query($myQuery);
+		$res = $res->fetch_object();
+
+		return $res->fecha_pago;
 	}
+}
